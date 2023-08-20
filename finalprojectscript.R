4e2cef154c87efc7eb1b19bfc485cebf1b3b5d51
jfelrice <jfelric@emory.edu>
2023-08-20 14:43:23 -0400

    made age statistic into mean

--- finalprojectscript.R
+++ finalprojectscript.R
@@ -1,11 +1,11 @@
 library(gtsummary)
 tbl_summary(
 	diabetes,
-	by = diabetes_5y,
 	include = everything(),
-	label=list(
-		select_all(
-		pregnancy_num ~ "# of Pregnancies",
+	statistic = list(all_continuous() ~ "{mean} ({sd})",
+									 all_categorical() ~ "{n} / {N} ({p}%)"),
+	label =list(
+		pregnancy_num ~ " num of Pregnancies",
 		glucose_mg-dl ~ "Glucose concentration(mg/dl)",
 		dbp_mm-hg ~ "dose of theophylline(mm/hg)",
 		triceps_mm ~"tricep thickness(mm)",
@@ -13,10 +13,31 @@ tbl_summary(
 		bmi ~ "BMI",
 		pedigree ~ "Pedigree score",
 		age ~ "Age (yrs)",
-		diabetes_5y ~ "Diagnosis of Diabetes after 5 years"),
+		diabetes_5y ~ "Diabetes"),
+	missing_text = ("Missing"))
 
-		missing_text = "Missing"),
+tbl_summary(
+	diabetes,
+	statistic = list(
+		all_continuous() ~ "{mean} ({sd})",
+		all_categorical() ~ "{n} / {N} ({p}%)",
+		age ~ "{mean}"
+	),
+	label = list(
+		pregnancy_num ~ "num of Pregnancies",
+		`glucose_mg-dl` ~ "Glucose concentration (mg/dl)",
+		`dbp_mm-hg` ~ "dose of theophylline (mm/hg)",
+		triceps_mm ~ "tricep thickness (mm)",
+		`insulin_microiu-ml` ~ "serum insulin levels (mL)",
+		bmi ~ "BMI",
+		pedigree ~ "Pedigree score",
+		age ~ "Age (yrs)",
+		diabetes_5y ~ "Diabetes"
+	),
+	missing_text = "Missing"
 )
+
+
 
 
 traceback()
