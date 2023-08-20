fea96bbd2521777c45248de7b2574177b5cd376d
jfelrice <jfelric@emory.edu>
2023-08-20 14:40:46 -0400

    fixed table labeling

--- finalprojectscript.R
+++ finalprojectscript.R
@@ -1,17 +1,22 @@
 library(gtsummary)
 tbl_summary(
 	diabetes,
+	by = diabetes_5y,
 	include = everything(),
-	label = list(
+	label=list(
+		select_all(
 		pregnancy_num ~ "# of Pregnancies",
 		glucose_mg-dl ~ "Glucose concentration(mg/dl)",
 		dbp_mm-hg ~ "dose of theophylline(mm/hg)",
 		triceps_mm ~"tricep thickness(mm)",
 		insulin_microiu-ml ~ "serum insulin levels(mL)",
-		bmi ~"BMI",
+		bmi ~ "BMI",
 		pedigree ~ "Pedigree score",
-		age ~ "Age(yrs)",
-		diabetes_5y ~ "Diagnosis of Diabetes after 5 years"
-	),
-	missing_text = "Missing")
+		age ~ "Age (yrs)",
+		diabetes_5y ~ "Diagnosis of Diabetes after 5 years"),
 
+		missing_text = "Missing"),
+)
+
+
+traceback()
